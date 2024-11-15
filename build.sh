435c0f7c4da8b1a5d02dcd4e81702a1b671a3af2
Andreas Richter <708186+richtera@users.noreply.github.com>
2024-11-14 19:05:40 -0500

    fix: Rename and fix build.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cargo build
-cd packages/wasm-client && wasm-pack build --target web && mv pkg/wasm_client.js pkg/wasm_client.mjs && pnpm tsup && pnpm pack
+cd packages/vetkd-notes-client && wasm-pack build --target web && mv pkg/wasm_client.js pkg/wasm_client.mjs && pnpm tsup && pnpm pack
