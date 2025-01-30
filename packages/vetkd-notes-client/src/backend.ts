a2b196765d6d8b6d985e87ffb73496bce4011b7b
Andreas Richter <708186+richtera@users.noreply.github.com>
2025-01-30 17:18:18 -0500

    fix: Remove base implementation and use npm/crate

--- packages/vetkd-notes-client/src/backend.ts
+++
@@ -1,8 +0,0 @@
-export type {
-  _SERVICE,
-  EncryptedNote,
-  PrincipalRule,
-  HistoryEntry,
-} from "../../vetkd-notes-canister/vetkd_notes_canister.d.ts";
-export { idlFactory } from "../../vetkd-notes-canister/vetkd_notes_canister.mjs";
-export const ENCRYPTED_NOTES_CANISTER_ID = process.env.VETKD_NOTES_CANISTER_ID;
--- packages/vetkd-notes-client/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./vetkd_notes_client.js";
-export * from "./actor.js";
-export * from "./backend.js";
-export * from "./note.js";
-export * from "./sleep.js";
-export * from "./crypto.js";
-import init from "./vetkd_notes_client.js";
-export default init;
