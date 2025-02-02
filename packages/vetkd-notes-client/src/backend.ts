0c030f277dd74a8c3c3d69b9975a2a25f8b8f1f6
Andreas Richter <708186+richtera@users.noreply.github.com>
2025-02-02 13:28:28 -0500

    fix: Remove base implementation and use npm/crate
    fix: Startup
    
    fix: Upgrades

--- packages/vetkd-notes-client/src/backend.ts
+++
@@ -1,8 +0,0 @@
-export type {
-  _SERVICE,
-  EncryptedNote,
-  PrincipalRule,
-  HistoryEntry,
-} from "./declarations/vetkd_notes/vetkd_notes.did.d.ts";
-export { idlFactory } from "./declarations/vetkd_notes/vetkd_notes.did.js";
-export const ENCRYPTED_NOTES_CANISTER_ID = process.env.VETKD_NOTES_CANISTER_ID;
