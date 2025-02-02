cca363e5134a7857dd06f0b9287c252e112575fe
Andreas Richter <708186+richtera@users.noreply.github.com>
2025-02-02 12:50:05 -0500

    fix: _SERVICE import changed

--- src/declarations/vetkd_system_api/index.d.ts
+++ src/declarations/vetkd_system_api/index.d.ts
@@ -7,7 +7,7 @@ import type {
 import type { Principal } from "@dfinity/principal";
 import type { IDL } from "@dfinity/candid";
 
-import { _SERVICE } from './vetkd_system_api.did';
+import type { _SERVICE } from "./vetkd_system_api.did.d.ts";
 
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const canisterId: string;
